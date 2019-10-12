b89b0d34c131abca1c44f14f3cd4a9feff6e60ca
Felix Koch <fkoch@student.tgm.ac.at>
2019-10-12 23:02:22 +0100

    Fortschritt???

--- test.php
+++ test.php
@@ -2,24 +2,26 @@
 $servername = "localhost";
 $username = "root";
 $password = "dataadmin";
+$dbname = "test";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
  
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+echo "<br>";
 
-// Datenbank erstellen
-$sql = "CREATE DATABASE myDB";
-if($conn->query($sql) === TRUE) {
-    echo "DB created";
-}
-else {
-    echo "error: " . $conn->error;
-}
+// Query:
+$sql = "INSERT INTO users (email, passwort, vorname, nachname) VALUES ('t2@t2.t2', 'pw2', 'v2', 'n2' )";
 
+// run query
+if ($conn->query($sql) === TRUE) {
+    echo "Query Successful";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 $conn->close();
 ?> 
