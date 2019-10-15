ed99bb9873c0bb790e571d25028bdc8a64717312
Felix Koch <fkoch@student.tgm.ac.at>
2019-10-15 20:40:58 +0100

    alles bis auf Insert

--- insert_data.php
+++ insert_data.php
@@ -12,14 +12,41 @@ if ($conn->connect_error) {
     die("<h2>CONNECT ERROR</h2>" . $conn->connect_error);
 }
 
-$sql = "SELECT password  FROM users where username=" . $_POST["user"].";";
+echo "connection established <br>";
 
-if($result = $conn->query($sql) == FALSE) {
-    die("<h2>QUERY ERROR: </h2>" . $conn->connect_error);
+
+
+//Save all the Data
+$sql = "SELECT shortname FROM produkte";
+$result = $conn->query($sql);
+if($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "<br> shortname: " . $row['shortname'];
+        echo" - " . $_POST[$row['shortname']];
+        //create long SQL-String with inserts
+    }
 }
+else {
+    echo "<br>no products";
+}
+
+
 //validate user
-$pwd = $result->fetch_assoc();
-echo $pwd;
+$sql = "SELECT * FROM users"; 
+$result = $conn->query($sql);
+if($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "<br>user: " . $row['username'] . " pwd: " . $row['password'];
+        if($_POST['user'] == $row['username'] && $_POST['pwd'] == $row['password']) {
+            echo "<br>user authenticated";
+            //INSERT the new Data^
+        }
+    }
+}
+else {
+    die("<h2>DATABASE ERROR: EMPTY</h2>");
+}
+
 
 //header("Location:home.php?success=true");
 ?>
