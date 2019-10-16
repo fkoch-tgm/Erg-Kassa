b69d1d371813df678cf09c9f4ae5d7d6911cd177
Felix Koch <fkoch@student.tgm.ac.at>
2019-10-16 18:58:19 +0100

    Kann Fehlermeldung zeigen

--- login.php
+++ login.php
@@ -8,21 +8,31 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> 
 </head>
-<body>
-<div class="container">
-<form method="post" action="/enter-session.php">
-<div class="input-group mb-3">
+<body style="height: 100vh;">
+<?php
+if($_GET['success'] == 'false') {
+    echo "
+    <div class='fixed-top'>
+        <div class='alert alert-danger mt-2 ml-5 mr-5'>
+            <strong>Fehler!</strong> Falsche/Keine Anmeldung.
+        </div>
+    </div>
+        ";
+}
+?>
+<div style="height: 100%;" class="container d-flex flex-column justify-content-center">
+<form action="/enter-session.php" class="align-self-center" method="post">
+<div class="input-group mb-3 align-self-center input-group-lg">
     <div class="input-group-prepend">
         <span class="input-group-text">Login</span>
     </div>
-    <input type=text class="form-control" placeholder="Benutzername" name="user">
-    <input type=password class="form-control" placeholder="Passwort" name="pwd">
+    <input type="text" class="form-control" placeholder="Benutzername" name="user">
+    <input type="password" class="form-control" placeholder="Passwort" name="pwd">
     <div class="input-group-append">
-        <button class="btn btn-success" type=submit>Weiter</button>
+        <button class="btn btn-success" type="submit">Weiter</button>
     </div>
 </div>
 </form>
-<?php echo $_SESSION['user']; ?>
 </div>
 </body>
 </html>
