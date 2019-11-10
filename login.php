94fed925e8504796f9963ff156fa73329c1ae6ce
Felix Koch <fkoch@student.tgm.ac.at>
2019-11-10 21:44:10 +0100

    github link

--- login.php
+++ login.php
@@ -34,5 +34,24 @@ if($_GET['success'] == 'false') {
 </div>
 </form>
 </div>
+<footer class="fixed-bottom text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <p class="text-muted">
+                    Created and Maintained by <a id="ghlink" data-toggle="tooltip" title="Github-Profil" href="https://github.com/fkoch-tgm/">Felix Koch</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- javascript -->
+<script type="text/javascript">
+   $('#ghlink').tooltip({ boundary: 'window' }); 
+</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> 
 </body>
 </html>
