db7055d3aa195ee1abc62a42230c2c40f42f7d9d
Felix Koch <fkoch@student.tgm.ac.at>
2020-01-20 11:04:34 +0100

    date picker

--- showdata.php
+++ showdata.php
@@ -1,13 +1,82 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <!-- Date Picker -->
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+
     <title>data</title>
-    
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
 <div class="container">
+    <div class="row">
+        <form action="showdata.php" method="get">
+        <div class="col-12 mt-4">
+            <div id="dateContainer" class="input-group mb-3 align-self-center input-group-lg">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Datum</span>
+                </div>
+                <input id="date" type="text" class="form-control" placeholder="Von-Bis">
+                <div class="input-group-append">
+                    <button class="btn-lg" type="submit">Weiter</button>
+                </div>
+            </div>
+
+            <script>
+                $(function() {
+                    $('#date').daterangepicker({
+                        timePicker: true,
+                        startDate: moment().startOf('hour'),
+                        timePicker24Hour: true,
+                        endDate: moment().startOf('hour').add(1,'day'),
+                        locale: {
+                            format: 'DD.MM.YYYY (hh:mm)',
+                            "separator": " - ",
+                            "applyLabel": "Bestätigen",
+                            "cancelLabel": "Abbrechen",
+                            "fromLabel": "Von",
+                            "toLabel": "Bis",
+                            "daysOfWeek": [
+                                "So",
+                                "Mo",
+                                "Di",
+                                "Mi",
+                                "Do",
+                                "Fr",
+                                "Sa"
+                            ],
+                            "monthNames": [
+                                "Jänner",
+                                "Februar",
+                                "März",
+                                "April",
+                                "Mai",
+                                "Juni",
+                                "Juli",
+                                "August",
+                                "September",
+                                "Oktober",
+                                "November",
+                                "Dezember"
+                            ],
+                        },
+                        firstDay: 1,
+                        parentEl: "#dateContainer"
+                    }, function (start, end, label) {
+                           alert(start + "-" + end); 
+                           console.log(start + "-" + end);
+                        }
+                    );
+                });
+            </script>
+        </div>
+        </form>
+    </div>
+
 <h1>Data:</h1>
 
 <div class="row">
